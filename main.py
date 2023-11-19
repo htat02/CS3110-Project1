ad136c8767acf01979b233e52859370ae522897b
Henry Tat <henrytat@Henrys-MacBook-Pro.local>
2023-11-18 22:26:15 -0800

    Added exponent and floating point

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 def is_valid_literal(input_string):
+    #remove underscore
+    input_string = input_string.replace('', '')
+
+    #check for sign
+    if input_string and (input_string[0] == '+' or input_string[0] == '-'):
+        input_string = input_string[1:]
+
+    #check for exponent
+    if 'e' in input_string.lower():
+        parts = input_string.lower().split('e')
+        if len(parts) != 2:
+            return "Invalid Literal"
+        base, exponent = parts
+        if (base.isdigit() or ('.' in base and all(c.isdigit() for c in base.replace('.', '', 1)))) and exponent.isdigit():
+            return "Floating Point Literal"
 
     #check for exponent
     if 'e' in input_string.lower():
@@ -18,10 +33,11 @@ def is_valid_literal(input_string):
     #hexadecimal integer literal 
     elif input_string[:2] == '0x' and all((c.isdigit() or c.lower() in 'abcdef') for c in input_string[2:]):
         return "Hexadecimal Integer Literal" 
-    
+      #floating point literal 
+    elif ('.' in input_string) and all(c.isdigit() or c in 'eE+-._' for c in input_string):
+        return "Floating Point Literal"
     else: 
         return "Invalid Literal"
-    
 
 input_string = input("Enter a string: ")
 print(is_valid_literal(input_string))
